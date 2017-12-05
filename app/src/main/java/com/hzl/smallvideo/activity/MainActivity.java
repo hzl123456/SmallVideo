e35fe3cacb61f3c8b76a2abaea23796067ddd3b6
请叫我百米冲刺 <1576044139@qq.com>
2017-12-05 14:16:51 +0800

    修改

--- app/src/main/java/com/hzl/smallvideo/activity/MainActivity.java
+++ app/src/main/java/com/hzl/smallvideo/activity/MainActivity.java
@@ -8,17 +8,12 @@ import android.media.MediaPlayer;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
-import android.os.Handler;
-import android.os.Looper;
 import android.support.annotation.NonNull;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.ImageView;
 import android.widget.VideoView;
 
-import com.github.hiteshsondhi88.libffmpeg.ExecuteBinaryResponseHandler;
-import com.github.hiteshsondhi88.libffmpeg.FFmpeg;
-import com.github.hiteshsondhi88.libffmpeg.exceptions.FFmpegCommandAlreadyRunningException;
 import com.hzl.smallvideo.R;
 import com.hzl.smallvideo.application.MainApplication;
 import com.hzl.smallvideo.listener.CameraPictureListener;
@@ -28,7 +23,6 @@ import com.hzl.smallvideo.manager.RecordManager;
 import com.hzl.smallvideo.manager.camera.CameraSurfaceView;
 import com.hzl.smallvideo.manager.camera.CaptureButton;
 import com.hzl.smallvideo.util.AppUtil;
-import com.hzl.smallvideo.util.BitmapUtil;
 import com.hzl.smallvideo.util.CommonUtil;
 import com.hzl.smallvideo.util.PermissionsUtils;
 import com.hzl.smallvideo.view.WatermarkView;
@@ -180,8 +174,14 @@ public class MainActivity extends Activity implements View.OnClickListener {
 
             @Override
             public void getRecordResult() { //需要录制
-                //进行水印添加的操作，然后保存本地
-                addDefaultWatermark();
+                CommonUtil.disMissDialog();
+                CommonUtil.showToast("视频保存成功");
+                //取消视频的播放
+                mVideoView.stopPlayback();
+                mVideoView.setVisibility(View.GONE);
+                //显示顶部的按钮
+                mBtnCamera.setVisibility(View.VISIBLE);
+                mBtnLight.setVisibility(View.VISIBLE);
             }
 
             @Override
@@ -215,61 +215,61 @@ public class MainActivity extends Activity implements View.OnClickListener {
         });
     }
 
-    public void addDefaultWatermark() {
-        //获取默认的水印信息并且保存为本地的png图片
-        final String waterPath = Environment.getExternalStorageDirectory().getPath() + File.separator + "water.png";
-        Bitmap bitmap = BitmapUtil.getDefaultWatermarkBitmap();
-        AppUtil.saveBitmapToFile(bitmap, waterPath);
-        //进行水印的添加
-        CommonUtil.showDialog("正在保存视频");
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                final String outPath = Environment.getExternalStorageDirectory().getPath() + File.separator + System.currentTimeMillis() + ".mp4";
-                //直接使用命令行的方式来添加水印
-                String[] commands = new String[8];
-                commands[0] = "-i";
-                commands[1] = MainActivity.this.filePath;
-                commands[2] = "-i";
-                commands[3] = waterPath;
-                commands[4] = "-filter_complex";
-                commands[5] = String.format("overlay=main_w-overlay_w-20:main_h-overlay_h-20");
-                commands[6] = "-y";
-                commands[7] = outPath;
-                try {
-                    FFmpeg.getInstance(MainActivity.this).execute(commands, new ExecuteBinaryResponseHandler() {
-                        @Override
-                        public void onSuccess(String message) {
-                            new Handler(Looper.getMainLooper()).post(new Runnable() {
-                                @Override
-                                public void run() {
-                                    CommonUtil.disMissDialog();
-                                    CommonUtil.showToast("视频保存成功");
-                                    //删除没有水印的图片和水印图片
-                                    new File(waterPath).delete();
-                                    new File(MainActivity.this.filePath).delete();
-                                    //取消视频的播放
-                                    mVideoView.stopPlayback();
-                                    mVideoView.setVisibility(View.GONE);
-                                    //显示顶部的按钮
-                                    mBtnCamera.setVisibility(View.VISIBLE);
-                                    mBtnLight.setVisibility(View.VISIBLE);
-                                }
-                            });
-                        }
-
-                        @Override
-                        public void onFailure(String message) {
-                            CommonUtil.disMissDialog();
-                            CommonUtil.showToast("视频保存失败");
-                        }
-                    });
-                } catch (FFmpegCommandAlreadyRunningException e) {
-                    e.printStackTrace();
-                }
-            }
-        }).start();
-    }
+//    public void addDefaultWatermark() {
+//        //获取默认的水印信息并且保存为本地的png图片
+//        final String waterPath = Environment.getExternalStorageDirectory().getPath() + File.separator + "water.png";
+//        Bitmap bitmap = BitmapUtil.getDefaultWatermarkBitmap();
+//        AppUtil.saveBitmapToFile(bitmap, waterPath);
+//        //进行水印的添加
+//        CommonUtil.showDialog("正在保存视频");
+//        new Thread(new Runnable() {
+//            @Override
+//            public void run() {
+//                final String outPath = Environment.getExternalStorageDirectory().getPath() + File.separator + System.currentTimeMillis() + ".mp4";
+//                //直接使用命令行的方式来添加水印
+//                String[] commands = new String[8];
+//                commands[0] = "-i";
+//                commands[1] = MainActivity.this.filePath;
+//                commands[2] = "-i";
+//                commands[3] = waterPath;
+//                commands[4] = "-filter_complex";
+//                commands[5] = String.format("overlay=main_w-overlay_w-20:main_h-overlay_h-20");
+//                commands[6] = "-y";
+//                commands[7] = outPath;
+//                try {
+//                    FFmpeg.getInstance(MainActivity.this).execute(commands, new ExecuteBinaryResponseHandler() {
+//                        @Override
+//                        public void onSuccess(String message) {
+//                            new Handler(Looper.getMainLooper()).post(new Runnable() {
+//                                @Override
+//                                public void run() {
+//                                    CommonUtil.disMissDialog();
+//                                    CommonUtil.showToast("视频保存成功");
+//                                    //删除没有水印的图片和水印图片
+//                                    new File(waterPath).delete();
+//                                    new File(MainActivity.this.filePath).delete();
+//                                    //取消视频的播放
+//                                    mVideoView.stopPlayback();
+//                                    mVideoView.setVisibility(View.GONE);
+//                                    //显示顶部的按钮
+//                                    mBtnCamera.setVisibility(View.VISIBLE);
+//                                    mBtnLight.setVisibility(View.VISIBLE);
+//                                }
+//                            });
+//                        }
+//
+//                        @Override
+//                        public void onFailure(String message) {
+//                            CommonUtil.disMissDialog();
+//                            CommonUtil.showToast("视频保存失败");
+//                        }
+//                    });
+//                } catch (FFmpegCommandAlreadyRunningException e) {
+//                    e.printStackTrace();
+//                }
+//            }
+//        }).start();
+//    }
 
     public void startVideo(String videoPath) {
         if (mVideoView.isPlaying()) {
