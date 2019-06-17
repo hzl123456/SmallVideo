759a33eb4e55d28094b2ef25e30c8bd34f88520a
请叫我百米冲刺 <1576044139@qq.com>
2019-06-17 14:31:40 +0800

    更新

--- app/src/main/java/com/hzl/smallvideo/manager/camera/CameraSurfaceView.java
+++ app/src/main/java/com/hzl/smallvideo/manager/camera/CameraSurfaceView.java
@@ -7,7 +7,6 @@ import android.animation.ObjectAnimator;
 import android.content.Context;
 import android.hardware.Camera;
 import android.util.AttributeSet;
-import android.util.Log;
 import android.view.MotionEvent;
 import android.view.SurfaceHolder;
 import android.view.View;
